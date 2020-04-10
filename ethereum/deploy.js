448e2cf4bb22d9176ce8d64e0ff39759a1163054
Alberto Fernández Gómez <afernandezgom@gmail.com>
2020-04-10 17:00:07 +0200

    feat: first commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+const hdWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+// const compiledCampaign = require('./build/Campaign.json');
+
+const provider = new hdWalletProvider(
+  'cage library whip mask impact dial agree nose obvious slight quantum arrange',
+  'https://rinkeby.infura.io/v3/4700d961e88c469982d438ca6df90b4e'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attemping to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
